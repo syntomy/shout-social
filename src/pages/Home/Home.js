ac2f1f7574abcd97162dda9520c5190707357d9f
syntomy <divistern@outlook.com>
2022-07-11 11:00:15 +0200

    show more type of pagination

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,12 +9,33 @@ import ReactLoading from "react-loading";
 
 import { BiRefresh } from "react-icons/bi";
 
+const ShoutBox = ({ shout }) => {
+  return (
+    <div className="shout-box">
+      <div className="shouter-info">
+        <img src={shout.shouter_avatar_url} alt="Shouter's avatar" className="shouter-avatar" />
+        <p className="shouter-tag">{shout.shouter_username}</p>
+      </div>
+      <div className="shout-content">
+        {shout.content}
+      </div>
+      <div className="creation-date">{new Date(shout.created_at).toDateString() + " , " + new Date(shout.created_at).toLocaleTimeString()}</div>
+    </div>
+  )
+};
+
 function Home() {
   const [shouts, setShouts] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [stopIndex, setStopIndex] = useState(5);
+
+  const showMore = () => {
+    setStopIndex(stopIndex + 5);
+  };
 
   const fetchShouts = async () => {
     setLoading(true);
+    setStopIndex(5);
     const { data, error } = await SupabaseClient.from("shouts").select();
     if (error) {
       toast.error("Something went wrong with fetching shouts.");
@@ -22,10 +43,9 @@ function Home() {
     }
     else {
       if (data.length > 0 && data !== []) {
-        setShouts(data);
+        setShouts(data.reverse());
       }
-      else
-      {
+      else {
         setShouts(null);
       }
       setLoading(false);
@@ -49,23 +69,25 @@ function Home() {
           {shouts ?
             <>
               <h1>Recent shouts: </h1>
-              {shouts.map((shout, index) => (
-                <div key={index} className="shout-box">
-                  <div className="shouter-info">
-                    <img src={shout.shouter_avatar_url} alt="Shouter's avatar" className="shouter-avatar" />
-                    <p className="shouter-tag">{shout.shouter_username}</p>
-                  </div>
-                  <div className="shout-content">
-                    {shout.content}
-                  </div>
-                  <div className="creation-date">{new Date(shout.created_at).toDateString() + " , " + new Date(shout.created_at).toLocaleTimeString()}</div>
-                </div>
-              ))}
+              <div>Shouts found: {shouts.length}</div>
+              <button className="refresh-button" onClick={fetchShouts}><BiRefresh /> Refresh</button>
+              {shouts.map((shout, index) => {
+                if(index < stopIndex)
+                {
+                  return <ShoutBox shout={shout} key={index} />
+                }
+                else
+                  return <></>
+              })}
+              {shouts.length > stopIndex ?
+              <button className="show-btn" onClick={showMore}>Show more</button>
+              :
+              <p className="loaded-message">Loaded all shouts</p>
+              }
             </>
             :
             <h1>No one has shouted anything yet!</h1>
           }
-          <button className="refresh-button" onClick={fetchShouts}><BiRefresh /> Refresh</button>
         </>
       }
     </MainLayout>
